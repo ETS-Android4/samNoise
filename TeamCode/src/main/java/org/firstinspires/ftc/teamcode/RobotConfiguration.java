01355c3994f6cb248b22e2cff4035f420f6b6d06
Victoria Mercouris <vnmercouris@wpi.edu>
2021-02-10 16:58:37 -0600

    Finished config file for now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConfiguration.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -11,13 +12,73 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * This class is used to define all the specific hardware for the Ultimate Goal Season.
  *
  * This hardware class assumes the following device names have been configured on the robot:
- * Note:  All names are lower case and some have single spaces between words.
+ * Note:  All names are camel case with no spaces.
  *
- * Motor channel:  Left  drive motor:        "left_drive"
- * Motor channel:  Right drive motor:        "right_drive"
- * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Motor channel:  Left Front drive motor:          "leftFrontDrive"
+ * Motor channel:  Right Front drive motor:         "rightFrontDrive"
+ * Motor channel:  Left Back Front drive motor:     "leftBackDrive"
+ * Motor channel:  Right Back drive motor:          "rightBackDrive"
+ * Motor channel:  Shooter drive motor:             "Shooter"
  * Servo channel:  Servo to open left claw:  "left_hand"
  * Servo channel:  Servo to open right claw: "right_hand"
  */
 public class RobotConfiguration {
+
+    /* Public OpMode members. */
+    public DcMotor  leftFrontDrive   = null;
+    public DcMotor  rightFrontDrive  = null;
+    public DcMotor  leftBackDrive     = null;
+    public DcMotor  rightBackDrive = null;
+    public DcMotor  shooter = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public RobotConfiguration(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftFrontDrive  = hwMap.get(DcMotor.class, "leftFrontDrive");
+        rightFrontDrive = hwMap.get(DcMotor.class, "rightFrontDrive");
+        leftBackDrive   = hwMap.get(DcMotor.class, "leftBackDrive");
+        rightBackDrive = hwMap.get(DcMotor.class, "rightBackDrive");
+        shooter   = hwMap.get(DcMotor.class, "shooter");
+
+        //Temporary directions for drive train, change after testing if needed.
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        //Temporary directions for shooter based on previous file testing
+        shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Set all motors to zero power
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+        shooter.setPower(0);
+
+
+
+        // Set all motors to run without encoders.
+        // Change to RUN_USING_ENCODERS if encoders are installed.
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+    }
 }
+
